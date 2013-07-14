88aefc938a4223891500fcd3607547d85627be35
Martin Morgan <mtmorgan@fhcrc.org>
2013-07-14 15:16:04 +0000

    pass ... of latex() to options, default width=90
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocStyle@78432 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/latex.R
+++ R/latex.R
@@ -1,6 +1,7 @@
 latex <-
-    function(...)
+    function(..., width=90)
 {
+    options(..., width=width)
     fl <- system.file(package="BiocStyle", "sty", "Bioconductor.sty")
     cat(sprintf("\\usepackage{%s}", sub(".sty$", "", fl)))
 }

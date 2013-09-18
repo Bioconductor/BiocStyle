c0255a9ca23eb13b5fc9dd4b192d26a2eae5578d
Andrzej Oleś <andrzej.oles@gmail.com>
2013-09-18 10:53:40 +0000

    version 0.0.19 - improved 'knitr' compatibility by adding:   opts_knit$set(latex.options.color = "usenames,dvipsnames") - added 'short.figlabels' option - corrected typo in the vignette: default latex font size is 10pt not 11pt
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocStyle@80541 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/latex.R
+++ R/latex.R
@@ -1,7 +1,39 @@
 latex <-
-    function(..., width=90)
+    function(..., width=90, short.figlabels=FALSE, fig.path)
 {
     options(..., width=width)
     fl <- system.file(package="BiocStyle", "sty", "Bioconductor.sty")
-    cat(sprintf("\\usepackage{%s}", sub(".sty$", "", fl)))
+    cat(sprintf("\\RequirePackage{%s}\n", sub(".sty$", "", fl)))
+    
+    setPrefix = function(x) {
+      cat(sprintf("\\renewcommand{\\prefix}{%s}", x))
+    }
+    
+    ## check whether called from knitr
+    fs = sapply(sys.calls(), function(x) as.character(x)[1])
+    id = match("knit", fs, nomatch=0)
+    
+    ## knitr
+    if ( id > 0 ) {
+      if( missing(fig.path) ){ 
+        fig.path = opts_chunk$get("fig.path", default=TRUE)
+#         ## resolve document file name for figure name prefixing
+#         filepath = sys.frame(id)$input
+#         filename = unlist(strsplit(basename(filepath), split=".", fixed=TRUE))
+#         filename = paste(filename[-length(filename)], collapse=".")
+#         fig.path = paste(filename, "-", sep="")
+      }
+      
+      if ( isTRUE(short.figlabels) ) setPrefix(fig.path)
+        
+      ## set knitr options
+      opts_knit$set(latex.options.color="usenames,dvipsnames")
+      opts_chunk$set(fig.path=fig.path) 
+    }
+    
+    ## assume sweave
+    else {
+      if ( isTRUE(short.figlabels) ) setPrefix("\\jobname-") 
+    }
+    
 }

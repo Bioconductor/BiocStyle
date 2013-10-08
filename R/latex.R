769634317501d0622af15d63256effc46bf123b7
Martin Morgan <mtmorgan@fhcrc.org>
2013-10-08 14:43:02 +0000

    BiocStyle::latex() should be sufficient to use style
    - can't assume knitr loaded already
    - need special code chunk for knit VignetteBuilder
    - reference man page for options rather than repeating in doc
    - formatting: hard-wrap to <80 columns, code indent 4 chars
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocStyle@81289 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/latex.R
+++ R/latex.R
@@ -14,21 +14,22 @@ latex <-
     id = pmatch("knit", fs, nomatch=0)
     
     ## knitr
-    if ( id > 0 ) {
-      if( missing(fig.path) ){ 
-        fig.path = opts_chunk$get("fig.path", default=TRUE)
-#         ## resolve document file name for figure name prefixing
-#         filepath = sys.frame(id)$input
-#         filename = unlist(strsplit(basename(filepath), split=".", fixed=TRUE))
-#         filename = paste(filename[-length(filename)], collapse=".")
-#         fig.path = paste(filename, "-", sep="")
-      }
-      
-      if ( isTRUE(short.fignames) ) setPrefix(fig.path)
+    if ( id > 0 && exists("opts_chunk")) {
+        if (missing(fig.path)) { 
+            fig.path = opts_chunk$get("fig.path", default=TRUE)
+            ## ## resolve document file name for figure name prefixing
+            ## filepath = sys.frame(id)$input
+            ## filename = unlist(strsplit(basename(filepath),
+            ##   split=".", fixed=TRUE))
+            ## filename = paste(filename[-length(filename)], collapse=".")
+            ## fig.path = paste(filename, "-", sep="")
+        }
         
-      ## set knitr options
-      opts_knit$set(latex.options.color="usenames,dvipsnames")
-      opts_chunk$set(fig.path=fig.path) 
+        if ( isTRUE(short.fignames) ) setPrefix(fig.path)
+        
+        ## set knitr options
+        opts_knit$set(latex.options.color="usenames,dvipsnames")
+        opts_chunk$set(fig.path=fig.path) 
     }
     
     ## assume Sweave

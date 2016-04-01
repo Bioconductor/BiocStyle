be2523bfc4fe66ec211fdc0033f63b001b2ec2b6
Andrzej Oleś <andrzej.oles@gmail.com>
2016-04-01 14:00:51 +0000

    BiocStyle2 - check in updates missing from the previous commit
    From: aoles <andrzej.oles@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocStyle@115622 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pdf_document2.R
+++ R/pdf_document2.R
@@ -1,21 +1,19 @@
 pdf_document2 <- function(toc = TRUE,
-                         number_sections = TRUE,
-                         fig_width = 7.5,
-                         fig_height = 5.0,
-                         fig_caption = TRUE,
-                         use.unsrturl = TRUE,
-                         includes,
-                         keep_md = FALSE,
-                         keep_tex = FALSE,
-                         toc_newpage = FALSE,
-                         titlecaps = TRUE,
-                         width = 68,
-                         ...) {
+                          number_sections = TRUE,
+                          fig_width = 7.5,
+                          fig_height = 5.0,
+                          fig_caption = TRUE,
+                          use.unsrturl = TRUE,
+                          includes,
+                          keep_md = FALSE,
+                          keep_tex = FALSE,
+                          toc_newpage = FALSE,
+                          titlecaps = TRUE,
+                          width = 68,
+                          ...) {
   
   ## load the package to expose macros
   require(BiocStyle, quietly = TRUE)
-  
-  options(..., width=width)
   
   ## get and modify the default pandoc template
   
@@ -48,7 +46,7 @@ pdf_document2 <- function(toc = TRUE,
     } else {
       to = grep(to, lines, fixed=TRUE)
     }
-      
+    
     # exit if nothing found
     if (length(to)==0)
       return(lines)
@@ -71,12 +69,23 @@ pdf_document2 <- function(toc = TRUE,
   lines <- substituteLines(lines, "\\hypersetup{linkcolor=black}")
   lines <- substituteLines(lines, "\\hypersetup{linkcolor=$if(toccolor)$$toccolor$$else$black$endif$}") # >= 1.14
   
+  # load BiocStyle after 'titling' to override title page formating, but before 
+  # author specification to ensure 'hyperref' gets loaded before 'authblk'
+  lines <- substituteLines(lines, "\\usepackage{titling}", replace = FALSE, to.offset = 1L,
+                           insert = c(loadBioconductorStyleFile(titlecaps = titlecaps), ""))
+  
+  # add author affiliations
+  lines <- substituteLines(lines, "\\author{$for(author)$$author$$sep$ \\\\ $endfor$}",
+                           insert = c("$for(author)$",
+                                      "$if(author.name)$  \\author{$author.name$}$else$  \\author{$author$}$endif$",
+                                      "$if(author.affiliation)$  \\affil{$author.affiliation$}$endif$$endfor$"))
+  
   # add package version number
   lines <- substituteLines(lines, "\\end{abstract}", replace = FALSE,
-                          insert = c("",
-                                     "$if(package)$",
-                                     "\\package{$package$}",
-                                     "$endif$"))
+                           insert = c("",
+                                      "$if(package)$",
+                                      "\\package{$package$}",
+                                      "$endif$"))
   
   # remove highlighting-macros
   lines <- substituteLines(lines, "$highlighting-macros$", from.offset = -1L, to.offset = 1L)
@@ -90,7 +99,7 @@ pdf_document2 <- function(toc = TRUE,
   
   writeLines(lines, template)
   
-  head = loadBioconductorStyleFile(titlecaps = titlecaps)
+  head = NULL
   
   ## code chunks and code highlighting
   thm <- system.file("themes", "default.css", package = "BiocStyle")
@@ -114,7 +123,29 @@ pdf_document2 <- function(toc = TRUE,
     includes = inc
   else
     includes$in_header = c(includes$in_header, inc)
-
+  
+  
+  # pandoc options
+  pandoc_args = NULL
+  
+  if (isTRUE(toc_newpage)) pandoc_args = c("-M", "toc-newpage")
+  
+  # knitr options
+  knitr = rmarkdown::knitr_options(
+    opts_knit = list(width = width),
+    opts_chunk = list(collapse=TRUE),
+    knit_hooks = list(
+      plot = function(x, options = list()) {
+        # adjust width for plots which are not inserted as floats
+        if (!length(options$fig.cap) || is.na(options$fig.cap)) {
+          paste0('\\begin{adjustwidth}{\\fltoffset}{0mm}',
+                 knitr::hook_plot_tex(x, options),
+                 '\\end{adjustwidth}')
+        } else {
+          knitr::hook_plot_md(x, options)
+        }
+      })
+  )
   
   # pandoc options
   pandoc_args = NULL
@@ -143,15 +174,15 @@ pdf_document2 <- function(toc = TRUE,
                            keep_md = keep_md,
                            clean_supporting = !keep_tex,
                            base_format = rmarkdown::pdf_document(
-                            toc = toc,
-                            number_sections = number_sections,
-                            fig_width = fig_width,
-                            fig_height = fig_height,
-                            fig_caption = fig_caption,
-                            template = template,
-                            includes = includes,
-                            keep_tex = keep_tex,
-                            pandoc_args = pandoc_args,
-                            ...)
+                             toc = toc,
+                             number_sections = number_sections,
+                             fig_width = fig_width,
+                             fig_height = fig_height,
+                             fig_caption = fig_caption,
+                             template = template,
+                             includes = includes,
+                             keep_tex = keep_tex,
+                             pandoc_args = pandoc_args,
+                             ...)
   )
 }

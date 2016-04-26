35552c7f61e57c6c4d390898fa257779284f9b57
Andrzej Oleś <andrzej.oles@gmail.com>
2016-04-26 16:02:01 +0000

    BiocStyle2 - revert original "Bioconductor LaTeX Style" vignette to old style - add "Bioconductor LaTeX Style 2.0" vignette
    From: aoles <andrzej.oles@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocStyle@116792 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,9 @@
-resources <- bioconductor.sty <- bioconductor.css <- NULL
+resources <- bioconductor.sty <- bioconductor2.sty <- bioconductor.css <- NULL
 
 # resolve paths once during package load
 .onLoad <- function(...) {
     resources <<- system.file(package = "BiocStyle", "resources")
     bioconductor.sty <<- file.path(resources, "tex", "Bioconductor.sty")
+    bioconductor2.sty <<- file.path(resources, "tex", "Bioconductor2.sty")
     bioconductor.css <<- file.path(resources, "html", "bioconductor.css")
 }

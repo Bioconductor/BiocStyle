f2b57ee2a5ad144d718d5888e1144809045cddbe
Andrzej Oleś <andrzej.oles@gmail.com>
2017-07-03 14:48:10 +0000

    version 2.5.6 - 'html_fragment': set figure and table captions as sidenotes, and refactor code
    From: aoles <andrzej.oles@gmail.com>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocStyle@130901 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/html_fragment.R
+++ R/html_fragment.R
@@ -7,58 +7,93 @@ html_fragment <- function(...,
   ## enable BiocStyle macros
   require(BiocStyle, quietly = TRUE)
   
-  # call rmarkdown::html_document with "default" template and substitute it only
-  # afterwards in order to retain original mathjax functionality
-  output_format_base <- rmarkdown::html_document(...,
-                           toc = toc,
-                           number_sections = number_sections,
-                           theme = theme,
-                           highlight = highlight)
-
-  template = system.file("resources", "fragment.html", package="BiocStyle")
-  idx = which(output_format_base$pandoc$args=="--template") + 1L
-  output_format_base$pandoc$args[idx] <- template
+  knitr <- knitr_options(opts_knit = list(width = 80L), 
+                         # remove figure margins
+                         opts_chunk = list(crop = TRUE),
+                         knit_hooks = list(crop = hook_pdfcrop))
   
-  base_format <- function(number_sections, output_format_base) {
+  post_processor <- function(metadata, input, output, clean, verbose) {
+    lines <- readUTF8(output)
     
-    post_processor <- function(metadata, input_file, output_file, clean, verbose) {
-      lines <- readUTF8(output_file)
-      
-      ## move all headers one level down (for proper formatting when embedded in the website)
-      template <- '<h%s>%s</h%s>'
-      pattern <- sprintf(template, '([1-5])', '(.*)', '\\1')
-      
-      headers <- grep(pattern, lines, value=TRUE)
-      levels <- as.integer(gsub(pattern, '\\1', headers))
-      
-      matches <- regexpr(pattern, lines)
-      
-      f <- function(header, level) {
-        sub(pattern, sprintf(template, level, '\\2', level), header)
-      }
-      
-      regmatches(lines, matches) <- mapply(f, headers, levels+1L, USE.NAMES = FALSE)
-      
-      ## add author affiliations
-      lines <- modifyLines(lines, from='<!-- AUTH AFFIL -->', insert=auth_affil_html(metadata))
-      
-      ## replace footnotes with sidenotes
-      lines = process_footnotes(lines)
-      
-      writeUTF8(lines, output_file)
-      output_file
-    }
+    ## move all headers one level down (for proper formatting when embedded in the website)
+    lines <- process_headers(lines)
     
-    rmarkdown::output_format(
-      knitr = NULL,
-      pandoc = NULL,
-      post_processor = post_processor,
-      base_format = output_format_base)
+    ## add author affiliations
+    lines <- modifyLines(lines, from='<!-- AUTH AFFIL -->', insert=auth_affil_html(metadata))
+    
+    writeUTF8(lines, output)
+    output
   }
   
-  bookdown::html_document2(
-    output_format_base = output_format_base,
-    number_sections = number_sections,
-    base_format = base_format
-  )
+  config <- rmarkdown::output_format(
+    knitr = knitr,
+    pandoc = NULL,
+    post_processor = post_processor,
+    base_format = bookdown::html_document2(
+      ...,
+      toc = toc,
+      number_sections = number_sections,
+      theme = theme,
+      highlight = highlight
+    ))
+  
+  ## swap template afterwards in order to retain original mathjax functionality
+  template <- system.file("resources", "fragment.html", package="BiocStyle")
+  idx <- which(config$pandoc$args=="--template") + 1L
+  config$pandoc$args[idx] <- template
+  
+  ## one needs to run the post processor processing headers before bookdown, but
+  ## process captions only after bookdown
+  
+  post = config$post_processor
+  
+  config$post_processor <- function(metadata, input, output, clean, verbose) {
+    if (is.function(post)) output = post(metadata, input, output, clean, verbose)
+    
+    lines <- readUTF8(output)
+    
+    ## replace footnotes with sidenotes
+    lines = process_footnotes(lines)
+    
+    ## set captions as sidenotes
+    lines = process_captions(lines)
+    
+    writeUTF8(lines, output)
+    output
+  }
+  
+  config
+}
+
+process_headers = function(lines) {
+  template <- '<h%s>%s</h%s>'
+  pattern <- sprintf(template, '([1-5])', '(.*)', '\\1')
+  
+  headers <- grep(pattern, lines, value=TRUE)
+  levels <- as.integer(gsub(pattern, '\\1', headers))
+  
+  matches <- regexpr(pattern, lines)
+  
+  f <- function(header, level) {
+    sub(pattern, sprintf(template, level, '\\2', level), header)
+  }
+  
+  regmatches(lines, matches) <- mapply(f, headers, levels+1L, USE.NAMES = FALSE)
+  
+  lines
+}
+
+process_captions = function(lines) {
+  ## convert table captions enlosed in <caption> tags into <p class="caption">
+  pattern <- '(^<caption>)(.*)(</caption>$)'
+  idx <- grep('^<table', lines)
+  idx <- idx[grepl(pattern, lines[idx+1L])]
+  captions <- lines[idx+1L]
+  lines[idx+1L] <- lines[idx]
+  lines[idx] <- gsub(pattern, '<p class="caption">\\2</p>', captions)
+  
+  ## append 'sidenote' CSS class to figure and table captions
+  lines = gsub('(?<=^<p class="caption)(?=">)', ' sidenote', lines, perl=TRUE)
+  
+  lines
 }

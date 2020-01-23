380fa078f77aa5db260bb26cfca77964ab7c3e86
Kayla-Morrell <Kayla.Morrell@roswellpark.org>
2020-01-23 13:41:37 -0500

    Improvements made to the `use_vignette_*()`
    - Rename `use_vignette_rmd()` to `use_vignette_html()`
    - Kept `use_vignette_pdf()` name as it was
    - Use of rmarkdown::draft() to write templates
    - Create an internal helper function to simplify exported functions
    - Improve stopifnot() to be sure getting proper input

--- R/use_vignette.R
+++ R/use_vignette.R
@@ -1,33 +1,44 @@
-#' Create vignettes that utilize the BiocStyle templates
+#' @importFrom rmarkdown draft
+.vignette_template <-
+    function(destination, mode = c("html", "pdf"))
+{
+    stopifnot(
+        is.character(destination), length(destination) == 1L, !is.na(destination),
+        !file.exists(destination), file.exists(dirname(destination))
+    )
+    mode <- match.arg(mode)
+
+    doc <- paste(mode, "document", sep = "_")
+    rmarkdown::draft(
+        destination, template = doc, package = "BiocStyle", edit = FALSE
+    )
+    destination
+}
+
+#' @title Create 'Rmd' vignette templates for HTML or PDF output
 #'
-#' Functions to create a skeleton Rmd or pdf vignette file.
 #' @rdname use_vignette
-#' @name use_vignette
-#' @param vignette_name character, the name of the vignette
-#' @return logical, indicating the vignette file was created successfully
+#' 
+#' @param destination character(1) file path to destination. The 
+#'     directory of the destination must already exist, and the file 
+#'     must not. The default creates a file in the temporary 
+#'     directory, and so is removed when the R session ends.
+#' 
 #' @examples
-#' use_vignette_rmd("testpkg_vignette")
+#' use_vignette_html()
+#'
 #' @export
-use_vignette_rmd <- function(vignette_name) {
-    stopifnot(is.character(vignette_name))
-
-    rmd = system.file(package = "BiocStyle", "rmarkdown", "templates", 
-        "html_document", "skeleton", "skeleton.Rmd")
-
-    file.copy(rmd, paste0(vignette_name, ".Rmd"))
+use_vignette_html <- 
+    function(destination = tempfile(fileext = ".Rmd")) 
+{
+    .vignette_template(destination, "html")
 }
 
 #' @rdname use_vignette
-#' @name use_vignette 
-#' @examples
 #'
-#' use_vignette_pdf("testpkg_vignette")
 #' @export
-use_vignette_pdf <- function(vignette_name) {
-    stopifnot(is.character(vignette_name))
-
-    rmd = system.file(package = "BiocStyle", "rmarkdown", "templates", 
-    "pdf_document", "skeleton", "skeleton.Rmd")
-
-    file.copy(rmd, paste0(vignette_name, ".pdf"))
+use_vignette_pdf <- 
+    function(destination = tempfile(fileext = ".Rmd")) 
+{
+    .vignette_template(destination, "pdf")
 }

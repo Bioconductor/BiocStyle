ea86852123a636ddf55116735b766936a140ed21
Martin Morgan <Martin.Morgan@RoswellPark.org>
2020-04-29 12:51:12 -0400

    reinstate original use_vignette_*(); introduce use_package_vignette_*()

--- R/use_vignette.R
+++ R/use_vignette.R
@@ -1,9 +1,88 @@
-#' @importFrom utils person
-#' @importFrom whisker whisker.render
+#' @rdname use_vignette
+#'
+#' @title Create 'Rmd' vignette templates for HTML or PDF output
+#'
+#' @name use_vignette
+#'
+#' @param destination character(1) file path to destination, usually
+#'     inside the `"vignettes"` directory of `package`. The directory
+#'     of the destination must already exist, and the file must
+#'     not. The default creates a file in the temporary directory, and
+#'     so is removed when the R session ends.
+#'
+#' @param package character(1) package name.
+#'
+#' @param title character(1) vignette title.
+#'
+#' @param abstract character(1) paragraph-length summary of the vignette.
+#'
+#' @param authors person() vignette authors. Use `person(comment =
+#'     ...)` to include author affiliation (see examples).
+#'
+#' @param bug_report_url character(1) url for bug reports (e.g.,
+#'     `"https://github.com/Bioconductor/BiocStyle/issues"`)
+#'
+#' @examples
+#' vignette <- use_package_vignette_html(
+#'     authors = c(
+#'         person(
+#'             "Iman", "Author",
+#'             email = "iman.author@some.where",
+#'             comment = c(affiliation = "Prestigious Institution")
+#'         ),
+#'         person("Iman", "Other")
+#'     )
+#' )
+#' vignette
+#' cat(readLines(vignette), "\n")
+NULL
+
+#' @importFrom rmarkdown draft
 .vignette_render <-
+    function(destination, output = c("html", "pdf"))
+{
+     stopifnot(
+         is.character(destination), length(destination) == 1L,
+         !is.na(destination),
+         !file.exists(destination), file.exists(dirname(destination))
+     )
+     output <- match.arg(output)
+
+     doc <- paste(output, "document", sep = "_")
+     draft(destination, template = doc, package = "BiocStyle", edit = FALSE)
+     destination
+}
+
+#' @rdname use_vignette
+#'
+#' @description `use_vignette_html()` and `use_vignette_pdf()` create
+#'     markdown vignettte templates for general use, in html or pdf
+#'     formats. The template outlines markdown syntax and unique
+#'     features enabled by BiocStyle.
+#'
+#' @export
+use_vignette_html <-
+    function(destination = tempfile(fileext = ".Rmd"))
+{
+    .vignette_render(destination, "html")
+}
+
+#' @rdname use_vignette
+#'
+#' @export
+use_vignette_pdf <-
+    function(destination = tempfile(fileext = ".Rmd"))
+{
+    .vignette_render(destination, "pdf")
+}
+
+#' @importFrom utils person
+#'
+#' @importFrom whisker whisker.render
+.package_vignette_render <-
     function(
         destination, package, title, abstract, authors, bug_report_url,
-        output
+        output = c("html", "pdf")
     )
 {
     stopifnot(
@@ -13,9 +92,9 @@
         is.character(title), length(title) == 1L,
         is.character(abstract), length(abstract) == 1L,
         inherits(authors, "person"),
-        is.character(bug_report_url), length(bug_report_url) == 1L,
-        is.character(output), length(output) == 1L
+        is.character(bug_report_url), length(bug_report_url) == 1L
     )
+    output <- match.arg(output)
 
     ## processing to list of authors, each author markdown formatted
     authors_data <- format(
@@ -55,44 +134,16 @@
     invisible(destination)
 }
 
-#' @title Create 'Rmd' vignette templates for HTML or PDF output
-#'
 #' @rdname use_vignette
 #'
-#' @param destination character(1) file path to destination, usually
-#'     inside the `"vignettes"` directory of `package`. The directory
-#'     of the destination must already exist, and the file must
-#'     not. The default creates a file in the temporary directory, and
-#'     so is removed when the R session ends.
-#'
-#' @param package character(1) package name.
-#'
-#' @param title character(1) vignette title.
-#'
-#' @param abstract character(1) paragraph-length summary of the vignette.
-#'
-#' @param authors person() vignette authors. Use `person(comment =
-#'     ...)` to include author affiliation (see examples).
-#'
-#' @param bug_report_url character(1) url for bug reports (e.g.,
-#'     `"https://github.com/Bioconductor/BiocStyle/issues"`)
-#'
-#' @examples
-#' vignette <- use_vignette_html(
-#'     authors = c(
-#'         person(
-#'             "Iman", "Author",
-#'             email = "iman.author@some.where",
-#'             comment = c(affiliation = "Prestigious Institution")
-#'         ),
-#'         person("Iman", "Other")
-#'     )
-#' )
-#' vignette
-#' cat(readLines(vignette), "\n")
+#' @description `use_package_vignette_html()` and
+#'     `use_package_vignette_pdf()` create vignette templates suitable
+#'     for inclusion in Bioconductor packages. The templates contain
+#'     package installation instructions, suggest overall structure,
+#'     and provide locations to seek support and create bug reports.
 #'
 #' @export
-use_vignette_html <-
+use_package_vignette_html <-
     function(
         destination = tempfile(fileext = ".Rmd"),
         package = "MyPackage",
@@ -102,15 +153,16 @@ use_vignette_html <-
         bug_report_url = "https://support.bioconductor.org"
     )
 {
-    .vignette_render(
+    .package_vignette_render(
         destination, package, title, abstract, authors, bug_report_url, "html"
     )
 }
 
+
 #' @rdname use_vignette
 #'
 #' @export
-use_vignette_pdf <-
+use_package_vignette_pdf <-
     function(
         destination = tempfile(fileext = ".Rmd"),
         package = "MyPackage",
@@ -120,7 +172,7 @@ use_vignette_pdf <-
         bug_report_url = "https://support.bioconductor.org"
     )
 {
-    .vignette_render(
+    .package_vignette_render(
         destination, package, title, abstract, authors, bug_report_url, "pdf"
     )
 }
